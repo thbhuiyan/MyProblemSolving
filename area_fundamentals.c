eef27a42f2b59a01ac4a0443f8778df23b81a215
thbhuiyan <th.bhuiyan124578@gmail.com>
2022-07-08 17:13:46 +0600

    assignment day 1 added

--- area_fundamentals.c
+++ area_fundamentals.c
@@ -1,3 +1,8 @@
+/*Assignment:
+1. Write a C program that takes three valid side lengths of a triangle as input.
+You must take inputs at integer data type.
+Print the area of that triangle with three digits after decimal point.
+Do typecasting if necessary.*/
 #include <stdio.h>
 #include<math.h>
 int main()
