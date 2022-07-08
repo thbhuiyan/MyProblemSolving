08d70b1a06767517f1e130d080c21d44bc716c60
thbhuiyan <th.bhuiyan124578@gmail.com>
2022-07-08 17:46:20 +0600

    4 solved

--- 4.cpp
+++ 4.cpp
@@ -1,2 +1,18 @@
 //4. Write a C program to check whether a given integer is odd or even.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,i=0;
+    while(i!=5)
+    {
+        cout<<endl<<"Enter a digit: ";
+        cin>>x;
+        if(x%2==0)
+            cout<<x<<" is an EVEN integer"<<endl;
+        else
+            cout<<x<<" is an ODD integer"<<endl;
+        i++;
+    }
+}
 
