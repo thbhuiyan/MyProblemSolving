02919e29c4bd2f6315114f96278e6ea424008cb6
thbhuiyan <th.bhuiyan124578@gmail.com>
2022-07-11 03:17:23 +0600

    9 solved

--- 9.cpp
+++ 9.cpp
@@ -1,1 +1,16 @@
 //9. Find the absolute difference between the two numbers.
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	int x,y,temp,i;
+	while(i!=6){
+	cout<<endl<<"Enter two digits: ";
+	cin>>x>>y;
+	if(x>y)
+	temp=abs(x-y);
+	else temp=abs(y-x);
+	cout<<temp;
+	i++;}
+}
