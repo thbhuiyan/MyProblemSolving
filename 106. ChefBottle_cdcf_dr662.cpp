f6e52abeaa09176376bfbc672b5f053d48890b2d
thbhuiyan <th.bhuiyan124578@gmail.com>
2022-07-18 22:22:56 +0600

    106 half solved

---
+++ 106. ChefBottle_cdcf_dr662.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,x,k,t,counter=0,store;
+    cin>>t;
+    while(i!=t)
+    {
+        cin>>n>>x>>k;
+        if( k>=(n*x) )
+        {cout<<n;
+        }
+        else if(){
+
+        }
+        return 0;
+    }
+}
