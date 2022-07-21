f60abb447b54e376348b65043db07f30df22459d
thbhuiyan <th.bhuiyan124578@gmail.com>
2022-07-22 03:01:25 +0600

    memorable 662 solved alhamdulillah

--- 106. ChefBottle_cdcf_dr662.cpp
+++ 106. ChefBottle_cdcf_dr662.cpp
@@ -1,18 +1,20 @@
+//Memorable problem of 2 day
+
 #include<iostream>
 using namespace std;
-int main()
-{
-    int i,n,x,k,t,counter=0,store;
+int main(){
+    int i=0,t;
     cin>>t;
-    while(i!=t)
-    {
-        cin>>n>>x>>k;
-        if( k>=(n*x) )
-        {cout<<n;
-        }
-        else if(){
-
-        }
-        return 0;
+    while(i!=t){
+    int x=0,n=0,k=0,count=0;
+    cin>>n>>x>>k;
+    while(k>=x && n!=0){
+        k=k-x;
+        count++;
+        n--;
     }
+    cout<<count<<endl;
+        i++;
+    }
+    return 0;
 }
